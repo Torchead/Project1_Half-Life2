9cea62c877b474b166bf0bf4331d5403d28c1067
Torchead <cbvbug@gmail.com>
2024-10-04 10:44:03 -0400

    Updates

--- javastuff.js
+++ javastuff.js
@@ -2,6 +2,7 @@
 /* multi line comment
 */
 
+// Combine image and sound, friend online image and sound, and device connected image and sound; respectively
 const combineimg = document.getElementById('combineimage')
 const combinewav = document.getElementById('combinesound')
 
@@ -11,6 +12,7 @@ const socialwav = document.getElementById('socialsound')
 const connectimg = document.getElementById('connectimage')
 const connectwav = document.getElementById('connectsound')
 
+// Same order, these are the events that trigger the sounds on click
 combineimg.addEventListener('click', () => {
     combinewav.play();})
     
